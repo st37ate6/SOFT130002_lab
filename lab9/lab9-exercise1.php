3ea8603e6f86772d29b8b5ccb93d9be6ee3e1028
st37ate6 <55232611+st37ate6@users.noreply.github.com>
2020-05-31 21:21:41 +0800

    Add files via upload

---
+++ lab9/lab9-exercise1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Exercise 1</title>
+</head>
+<body>
+<h1>Regular HTML section (outside &lt;?php ... ?&gt; tags)</h1>
+<p>You can type regular HTML here and it will show up</p>
+
+<h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
+<?php
+//this is a php comment IN tags (will not appear)
+$d = date("l, F dS, Y");
+echo "This page was generated: " . $d . "<hr/>";
+echo "<br>"; //notice we must echo tags in php.
+
+$remaining = 365 - date("z");
+$year = (int)date("Y");
+if ($year & 4 === 0) $remaining += 1;
+echo "There are ". $remaining . " days left in the year";
+
+?>
+</body>
+</html>
