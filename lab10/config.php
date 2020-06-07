bfa437741306ba2006aa0ea68b77d14497128507
st37ate6 <55232611+st37ate6@users.noreply.github.com>
2020-06-07 22:47:20 +0800

    Add files via upload

---
+++ lab10/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'fsy2001');
+define('DBPASS', '*******');
+define('DBCONNSTRING','mysql:dbname=art; charset=utf8mb4;'); ?>
